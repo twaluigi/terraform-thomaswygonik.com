cb40757a35bf8a406be9acec4c495f72a1819e98
EC2 Default User <thomas.wygonik@gmail.com>
2019-03-05 16:14:28 +0000

    updating backend to use state locking

--- backend.tf
+++ backend.tf
@@ -3,7 +3,9 @@ terraform {
     bucket = "tomwygonik-tfstate"
 
     # Can't set anything in here as a variable, backend needs to be initialized before variables
-    key    = "terraform/thomaswygonik.com/terraform-thomaswygonik-site.tfstate"
-    region = "us-east-2"
+    key            = "terraform/thomaswygonik.com/terraform-thomaswygonik-site.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "tomwygonik-tfstate-lock"
+    encrypt        = true
   }
 }
